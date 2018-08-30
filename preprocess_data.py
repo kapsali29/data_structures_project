865f90b9b827acc92101f643109bbb9f0895b723
Panagiotis Kapsalis <kapsalis@synelixis.com>
2018-08-30 16:57:12 +0300

    some changes

--- preprocess_data.py
+++ preprocess_data.py
@@ -5,6 +5,8 @@ def file_preprocessing(file, new_file):
     :param file: dataset
     :param new_file: file where the processed data will be stored
     :return:
+    Example
+        >>>file_preprocessing("cleared_dataset.csv", "final_dataset.csv")
     """
     with open(file, encoding="utf-8", errors='ignore') as file:
         lines = file.readlines()
@@ -19,4 +21,29 @@ def file_preprocessing(file, new_file):
                     continue
 
 
-file_preprocessing("cleared_dataset.csv", "final_dataset.csv")
+def create_test_dataset(file, new_file, data_size):
+    """
+    Using that function you are able to create a test dataset in order for better testing and debugging
+
+    :param file: initial dataset
+    :param new_file: test dataset filename
+    :param data_size: size of data
+    :return:
+    Example:
+        >>>create_test_dataset("final_dataset.csv", "test_dataset.csv", data_size=100)
+    """
+    with open(file, encoding="utf-8", errors='ignore') as file:
+        lines = file.readlines()
+        with open(new_file, "w") as f:
+            counter = 1
+            for line in lines:
+                if counter == data_size:
+                    break
+                else:
+                    counter += 1
+                    cleaned_data = [substring for substring in line.split("\"") if substring != ',' and substring != '']
+                    if cleaned_data:
+                        new_line = ",".join(cleaned_data)
+                        f.write(new_line)
+                    else:
+                        continue
